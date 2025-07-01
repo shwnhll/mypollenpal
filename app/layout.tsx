1d03db0c40f4928508cd9975dfaceaa6c2ef55eb
shwnhll <85854111+shwnhll@users.noreply.github.com>
2025-07-01 16:12:42 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
+    <head>
+      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤧</text></svg>" />
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
