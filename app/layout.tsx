64cbb8aa5c0f267c35127676fe4d60d6d9f80465
shwnhll <85854111+shwnhll@users.noreply.github.com>
2025-07-02 22:52:26 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
       <html lang="en">
     <head>
       <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤧</text></svg>" />
+      <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
     </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
