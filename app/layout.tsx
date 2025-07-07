4d7823987a9127a95896b6e68be07e2ba59430cc
shwnhll <85854111+shwnhll@users.noreply.github.com>
2025-07-07 11:26:42 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,15 @@ export default function RootLayout({
   return (
       <html lang="en">
     <head>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SZE8WFYZ81"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SZE8WFYZ81');
+</script>
       <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤧</text></svg>" />
       <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
     </head>
