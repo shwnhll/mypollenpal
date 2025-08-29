783f4463a3941dde156a9659255079fac5e0d440
shwnhll <85854111+shwnhll@users.noreply.github.com>
2025-08-29 16:33:28 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        // Redirect ZIP-city-state to city-state format
+        source: '/:zip(\\d{5})-:city-:state',
+        destination: '/:city-:state',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
